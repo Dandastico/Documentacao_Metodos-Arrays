61d375ff0b6142d13d7bc5bb1d2a51d77a167a56
Dandastico <dandastico.bsb@gmail.com>
2025-04-15 18:26:50 -0300

    término do js minmax

--- js/minmax.js
+++ js/minmax.js
@@ -3,7 +3,8 @@ console.log("JS minmax ativada!");
 // variáveis globais
 let opt1 = document.getElementById("output-1");
 let opt2 = document.getElementById("output-2");
-
+let opt3 = document.getElementById("output-3");
+let opt4 = document.getElementById("output-4");
 
 let quebra = "<br/>";
 let quebra2 = "<br/><br/>";
@@ -13,6 +14,8 @@ let con = ", ";
 function resetar() {
     opt1.innerHTML = quebra2+quebra2+quebra;
     opt2.innerHTML = quebra2+quebra;
+    opt3.innerHTML = quebra2+quebra2+quebra;
+    opt4.innerHTML = quebra2+quebra;
 }
 
 
@@ -32,6 +35,22 @@ function matematica2() {
 }
 
 
+function matematica3() {
+    let m = Math.max(10, 5, 8);
+    let n = NaN;
+    let inf = Math.max();
+
+    opt3.innerHTML = m+quebra2+Math.min(m,n)+quebra2+inf;
+}
+
+
+function matematica4() {
+    let media = (0 + 0) / 2 + (-1);
+
+    opt4.innerHTML = media+quebra2+Math.max(0, media);
+}
+
+
 // criação de observadores de eventos
 document.addEventListener("DOMContentLoaded", function() {
     // primeira seção do código com min()
@@ -40,4 +59,10 @@ document.addEventListener("DOMContentLoaded", function() {
     // segunda seção de código com min()
     document.getElementById("bt-ativar-min-2").addEventListener("click", matematica2);
     document.getElementById("bt-resetar-2").addEventListener("click", resetar);
+    // primeira seção de código com max()
+    document.getElementById("bt-ativar-max-1").addEventListener("click", matematica3);
+    document.getElementById("bt-resetar-3").addEventListener("click", resetar);
+    // segunda seção de código com max()
+    document.getElementById("bt-ativar-max-2").addEventListener("click", matematica4);
+    document.getElementById("bt-resetar-4").addEventListener("click", resetar);
 });
