91dac5ce623bf4bdf0590c504aaaf8c021d0ce97
Dandastico <dandastico.bsb@gmail.com>
2025-04-15 18:01:07 -0300

    término de HTML da página minmax

--- js/minmax.js
+++ js/minmax.js
@@ -2,6 +2,8 @@ console.log("JS minmax ativada!");
 
 // variáveis globais
 let opt1 = document.getElementById("output-1");
+let opt2 = document.getElementById("output-2");
+
 
 let quebra = "<br/>";
 let quebra2 = "<br/><br/>";
@@ -10,6 +12,7 @@ let con = ", ";
 
 function resetar() {
     opt1.innerHTML = quebra2+quebra2+quebra;
+    opt2.innerHTML = quebra2+quebra;
 }
 
 
@@ -22,9 +25,19 @@ function matematica1() {
 }
 
 
+function matematica2() {
+    let media = (10 +10) / 2 + 1;
+
+    opt2.innerHTML = media+quebra2+Math.min(10, media);
+}
+
+
 // criação de observadores de eventos
 document.addEventListener("DOMContentLoaded", function() {
     // primeira seção do código com min()
     document.getElementById("bt-ativar-min-1").addEventListener("click", matematica1);
     document.getElementById("bt-resetar-1").addEventListener("click", resetar);
+    // segunda seção de código com min()
+    document.getElementById("bt-ativar-min-2").addEventListener("click", matematica2);
+    document.getElementById("bt-resetar-2").addEventListener("click", resetar);
 });
