44a2fe1bda05494d21d8890a2ef59324cebe52c7
Dandastico <dandastico.bsb@gmail.com>
2025-04-15 00:36:56 -0300

    terminei o arqigo da página de sort() TODO: fazer a nav e footer

--- js/sort.js
+++ js/sort.js
@@ -1,6 +1,8 @@
 console.log("Arquivo JS abriu");
 
 let opt = document.getElementById("output-1");
+let opt2 = document.getElementById("output-2");
+let quebra = "<br/><br/>"
 
 function magica1() {
     let strings = ["daniel", "ama", "jordan"];
@@ -15,10 +17,29 @@ function magica1() {
 
 function resetar() {
     opt.innerHTML = '';
+    opt2.innerHTML = '';
 }
 
+function magica2() {
+    let nums = [100, 4, 20];
+    let nums2 = [100, 4, 20];
+    let nums3 = [100, 4, 20];
+    let original = [100, 4, 20];
+
+    nums.sort();
+    nums2.sort((a,b) => a-b);
+    nums3.sort((a,b) => b-a);
+
+
+    opt2.innerHTML = original.join(", ") + quebra+ nums.join(", ") + quebra + nums2.join(", ") + quebra + nums3.join(", ");
+}
+
+// criar observadores de eventos com os botões
 document.addEventListener('DOMContentLoaded', function() {
-    // criar observadores de eventos com os botões
-    document.getElementById("bt-ativar-sort1").addEventListener("click", magica1);
+    // primeira seção com códigos
+    document.getElementById("bt-ativar-sort-1").addEventListener("click", magica1);
     document.getElementById("bt-resetar-1").addEventListener("click", resetar);
+    // segunda seção com códigos
+    document.getElementById("bt-ativar-sort-2").addEventListener("click", magica2);
+    document.getElementById("bt-resetar-2").addEventListener("click", resetar);
 });
