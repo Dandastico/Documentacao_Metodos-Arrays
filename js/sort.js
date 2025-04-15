d23cd630899bb4a69c92365b19b09a976226a961
Dandastico <dandastico.bsb@gmail.com>
2025-04-15 06:12:34 -0300

    agora eu verdadeiramente terminei a página de sort()

--- js/sort.js
+++ js/sort.js
@@ -2,7 +2,11 @@ console.log("Arquivo JS abriu");
 
 let opt = document.getElementById("output-1");
 let opt2 = document.getElementById("output-2");
-let quebra = "<br/><br/>"
+let opt3 = document.getElementById("output-3");
+let quebra = "<br/>";
+let quebra2 = "<br/><br/>";
+let con = ", ";
+
 
 function magica1() {
     let strings = ["daniel", "ama", "jordan"];
@@ -12,13 +16,9 @@ function magica1() {
     console.log(original);
     console.log(strings);
 
-    opt.innerHTML = original.join(", ") + "<br />" + "<br />" + strings.join(", ");
+    opt.innerHTML = original.join(", ") + quebra2 + strings.join(con);
 }
 
-function resetar() {
-    opt.innerHTML = '';
-    opt2.innerHTML = '';
-}
 
 function magica2() {
     let nums = [100, 4, 20];
@@ -30,9 +30,27 @@ function magica2() {
     nums2.sort((a,b) => a-b);
     nums3.sort((a,b) => b-a);
 
-
-    opt2.innerHTML = original.join(", ") + quebra+ nums.join(", ") + quebra + nums2.join(", ") + quebra + nums3.join(", ");
+    opt2.innerHTML = original.join(con) + quebra2 + nums.join(con) + quebra2 + nums2.join(con) + quebra2 + nums3.join(con);
 }
+
+
+function magica3() {
+    let cidades = ["brasilia", "belo horizonte", "salvador", "porto seguro"];
+    let cidadesOrdenadas;
+    let original = ["brasilia", "belo horizonte", "salvador", "porto seguro"];
+
+    cidadesOrdenadas = cidades.sort();
+
+    opt3.innerHTML = original.join(con) + quebra2 + cidades.join(con) + quebra + cidadesOrdenadas.join(con);
+}
+
+
+function resetar() {
+    opt.innerHTML = quebra+quebra2;
+    opt2.innerHTML = quebra2+quebra2+quebra2+quebra;
+    opt3.innerHTML = quebra2+quebra2;
+}
+
 
 // criar observadores de eventos com os botões
 document.addEventListener('DOMContentLoaded', function() {
@@ -42,4 +60,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // segunda seção com códigos
     document.getElementById("bt-ativar-sort-2").addEventListener("click", magica2);
     document.getElementById("bt-resetar-2").addEventListener("click", resetar);
+    // terceira seão com códigos
+    document.getElementById("bt-ativar-sort-3").addEventListener("click", magica3);
+    document.getElementById("bt-resetar-3").addEventListener("click", resetar);
 });
