27f381fbb5ab179e773cd49ff3183a3c26a3ee95
Dandastico <dandastico.bsb@gmail.com>
2025-04-15 16:17:20 -0300

    térmido na página toSorted

--- js/toSorted.js
+++ js/toSorted.js
@@ -2,6 +2,7 @@ console.log("JS toSorted ativada!")
 
 // variáveis globais
 let opt1 = document.getElementById("output-1");
+let opt2 = document.getElementById("output-2");
 
 let quebra = "<br/>";
 let quebra2 = "<br/><br/>";
@@ -10,6 +11,7 @@ let con = ", ";
 
 function resetar() {
     opt1.innerHTML = quebra2+quebra;
+    opt2.innerHTML = quebra2+quebra2+quebra2+quebra;
 }
 
 
@@ -21,9 +23,22 @@ function magica1() {
 }
 
 
+function magica2() {
+    let nums = [100, 4, 20];
+    let numsOrdenadosStr = nums.toSorted();
+    let numsOrdenadosAsc = nums.toSorted((a,b) => a-b);
+    let numsOrdenadosDes = nums.toSorted((a,b) => b-a);
+
+    opt2.innerHTML = nums.join(con) + quebra2 + numsOrdenadosStr.join(con) + quebra2 + numsOrdenadosAsc.join(con) + quebra2 + numsOrdenadosDes.join(con)
+}
+
+
 // criação dos observadores de eventos
-document.addElementById("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
     // primeira seção com códigos
     document.getElementById("bt-ativar-tosorted-1").addEventListener("click", magica1);
     document.getElementById("bt-resetar-1").addEventListener("click", resetar);
+    // segunda seção com códigos
+    document.getElementById("bt-ativar-tosorted-2").addEventListener("click", magica2);
+    document.getElementById("bt-resetar-2").addEventListener("click", resetar);
 });
